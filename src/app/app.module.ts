f3367bb2fb6fc3b0332dfdb7b86f554f1e2cab2f
unknown <mittalshubham05@gmail.com>
2020-11-30 12:27:54 +0530

    Add: provided functionality for displaying employee data in table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import {HttpClientModule} from '@angular/common/http';
 import { EmployeeServiceService } from './service/employee-service.service';
 import { EmployeeListComponent } from './component/employee-list/employee-list.component';
 import { InvalidUrlComponent } from './component/invalid-url/invalid-url.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DemoMaterialModule } from './material-module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -19,7 +22,11 @@ import { InvalidUrlComponent } from './component/invalid-url/invalid-url.compone
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    DemoMaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [EmployeeServiceService],
   bootstrap: [AppComponent]
